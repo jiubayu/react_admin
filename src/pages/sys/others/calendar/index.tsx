b1547e1b64b4ed1ad722636a7d0956dd0e02cf6f
Tianbao <>
2025-11-11 17:24:57 +0800

    calendar complete

--- src/pages/sys/others/calendar/index.tsx
+++ src/pages/sys/others/calendar/index.tsx
@@ -4,15 +4,27 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
+import {
+  type DateSelectArg,
+  type EventClickArg,
+  type EventInput,
+} from '@fullcalendar/core';
+import dayjs from 'dayjs';
+import {faker} from '@faker-js/faker';
 
 import Card from '@/components/card';
-import CalendarHeader, {type HandleMoveArg, type ViewType} from './calendar-header';
+import CalendarHeader, {
+  type HandleMoveArg,
+  type ViewType,
+} from './calendar-header';
 import {useSettings} from '@/store/settingStore';
 import {StyledCalendar} from './styles';
 import {down, useMediaQuery} from '@/hooks';
 import {INITIAL_EVENTS} from './event-utils';
 import CalendarEvent from './calendar-event';
-import {type DateSelectArg} from '@fullcalendar/core';
+
+import type {CalendarEventFormFieldType} from './calendar-event-form';
+import CalendarEventForm from './calendar-event-form';
 
 const DefaultEventInitValue = {
   id: faker.string.uuid(),
@@ -29,7 +41,8 @@ function Calender() {
   const [date, setDate] = useState(new Date());
   const [view, setView] = useState<ViewType>('dayGridMonth');
   const [eventFormType, setEventFormType] = useState<'add' | 'edit'>('add');
-  const [eventInitValue, setEventInitValue] = useState<CalendarEventFromFieldType>(DefaultEventInitValue);
+  const [eventInitValue, setEventInitValue] =
+    useState<CalendarEventFormFieldType>(DefaultEventInitValue);
 
   const fullCalendarRef = useRef<FullCalendar>(null);
 
@@ -61,6 +74,7 @@ function Calender() {
     setView(viewType);
   };
 
+  // 处理日期选择事件
   const handleDateSelect = (selectInfo: DateSelectArg) => {
     const calendarApi = selectInfo.view.calendar;
     calendarApi.unselect(); // clear date selection
@@ -68,8 +82,102 @@ function Calender() {
     setOpen(true);
     setEventFormType('add');
     setEventInitValue({
+      id: faker.string.uuid(),
+      title: '',
+      description: '',
+      start: dayjs(selectInfo.startStr),
+      end: dayjs(selectInfo.endStr),
+      allDay: selectInfo.allDay,
+    });
+  };
 
-    })
+  // 处理事件点击事件
+  const handleEventClick = (arg: EventClickArg) => {
+    const {title, extendedProps, allDay, start, end, backgroundColor, id} =
+      arg.event;
+    setOpen(true);
+    setEventFormType('edit');
+
+    const newEventValue: CalendarEventFormFieldType = {
+      id,
+      title,
+      allDay,
+      description: extendedProps?.description || '',
+      color: backgroundColor || '',
+    };
+
+    if (start) {
+      newEventValue.start = dayjs(start);
+    }
+
+    if (end) {
+      newEventValue.end = dayjs(end);
+    }
+
+    setEventInitValue(newEventValue);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+    setEventInitValue(DefaultEventInitValue);
+  };
+
+  // 处理创建事件
+  const handleCreate = (values: CalendarEventFormFieldType) => {
+    const calendarApi = fullCalendarRef.current?.getApi();
+    if (!calendarApi) return;
+
+    const {title = '', description, start, end, allDay = false, color} = values;
+
+    const newEvent: EventInput = {
+      id: faker.string.uuid(),
+      title,
+      allDay,
+      color,
+      extendedProps: {
+        description,
+      },
+    };
+    if (start) newEvent.start = start.toDate();
+    if (end) newEvent.end = end.toDate();
+
+    // 刷新日历显示
+    calendarApi.addEvent(newEvent);
+  };
+
+  // 编辑日历
+  const handleEdit = (values: CalendarEventFormFieldType) => {
+    const calendarApi = fullCalendarRef.current?.getApi();
+    if (!calendarApi) return;
+
+    const { id, title = '', description, start, end, allDay = false, color } = values;
+    
+    const oldEvent = calendarApi.getEventById(id);
+
+    const newEvent: EventInput = {
+      id,
+      title,
+      allDay,
+      color,
+      extendedProps: {
+        description,
+      },
+    };
+    if (start) newEvent.start = start.toDate();
+    if (end) newEvent.end = end.toDate();
+
+    // 刷新日历显示
+    oldEvent?.remove();
+    calendarApi.addEvent(newEvent);
+  };
+
+  // 删除日历事件
+  const handleDelete = (id: string) => {
+    const calendarApi = fullCalendarRef.current?.getApi();
+    if (!calendarApi) return;
+
+    const event = calendarApi.getEventById(id);
+    event?.remove();
   };
 
   useEffect(() => {
@@ -94,7 +202,12 @@ function Calender() {
           />
           <FullCalendar
             ref={fullCalendarRef}
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
             initialDate={date}
             initialView={xsBreakPoint ? 'listWeek' : view}
             events={INITIAL_EVENTS}
@@ -109,6 +222,15 @@ function Calender() {
           />
         </StyledCalendar>
       </div>
+      <CalendarEventForm
+        type={eventFormType}
+        open={open}
+        onCancel={handleCancel}
+        onEdit={handleEdit}
+        onCreate={handleCreate}
+        onDelete={handleDelete}
+        initValues={eventInitValue}
+      />
     </Card>
   );
 }
